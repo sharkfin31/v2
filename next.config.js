973b389f521752a809c342d5eff04e31b273505c
sharkfin31 <shyamsravikumar@gmail.com>
2025-06-08 19:39:18 -0700

    chore: simplify next.config.js and add engines field in package.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/v2" : "",
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
